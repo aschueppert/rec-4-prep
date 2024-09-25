68dae73d94e33e7c07cd74a96a654a5d62303f8f
Amelia Scheppert <ameliascheppert@dhcp-10-29-177-78.dyn.MIT.EDU>
2024-09-25 16:35:17 -0400

    finish todos

--- server/concepts/authenticating.ts
+++ server/concepts/authenticating.ts
@@ -33,10 +33,7 @@ export default class AuthenticatingConcept {
   }
 
   async getUserById(_id: ObjectId) {
-    // TODO 1: implement this operation
-    //  - use this.users.readOne(..)
-    //  - don't include the password (we've provided a helper function you can use!)
-    throw new Error("Not implemented!");
+    return this.users.readOne({_id})
   }
 
   async getUsers(username?: string) {
@@ -55,10 +52,8 @@ export default class AuthenticatingConcept {
   }
 
   async updateUsername(_id: ObjectId, username: string) {
-    // TODO 2: implement this operation
-    //  - use this.users.partialUpdateOne(..)
-    //  - maintain the invariant that usernames are unique (we've provided a helper function!)
-    throw new Error("Not implemented!");
+    this.users.partialUpdateOne({_id}, {username})
+    return  { msg: "Successfully updated.",username};
   }
 
   async delete(_id: ObjectId) {
